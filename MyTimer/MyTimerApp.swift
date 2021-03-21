ed6f0de1545a11bb4a8dde9caef80e052ff21039
Chuck <cosmoc05@yahoo.com>
2021-03-21 14:10:48 -0700

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Charlie Caquias on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
